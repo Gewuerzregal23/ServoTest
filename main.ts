161b9c06c2edea3678d3c811d5218bc50505bc37
Gewuerzregal23 <155564302+Gewuerzregal23@users.noreply.github.com>
2025-02-12 23:46:44 +0100

    Display hinzu

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     Winkel += 10
+    SchreibeStatus()
     servos.C9.setAngle(Winkel)
     if (Winkel >= 180) {
         Winkel = 0
@@ -7,6 +8,7 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     Motor_ein = Motor_ein * -1
+    SchreibeStatus()
     if (Motor_ein == 1) {
         motors.dualMotorPower(Motor.M1, 15)
     } else {
@@ -15,18 +17,38 @@ input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     Servo_ein = Servo_ein * -1
+    SchreibeStatus()
     if (Servo_ein == 1) {
         servos.C8.run(50)
     } else {
         servos.C8.stop()
     }
 })
+function SchreibeStatus () {
+    oledssd1306.setTextXY(0, 12)
+    oledssd1306.clearRange(3)
+    oledssd1306.setTextXY(0, 12)
+    oledssd1306.writeNumber(Winkel)
+    oledssd1306.setTextXY(1, 12)
+    oledssd1306.clearRange(3)
+    oledssd1306.setTextXY(1, 12)
+    oledssd1306.writeNumber(Servo_ein)
+    oledssd1306.setTextXY(2, 12)
+    oledssd1306.clearRange(3)
+    oledssd1306.setTextXY(2, 12)
+    oledssd1306.writeNumber(Motor_ein)
+}
 let Winkel = 0
 let Motor_ein = 0
 let Servo_ein = 0
 Servo_ein = -1
 Motor_ein = -1
 Winkel = 0
-basic.forever(function () {
-	
-})
+oledssd1306.initDisplay()
+oledssd1306.setTextXY(0, 0)
+oledssd1306.writeString("Winkel(A):")
+oledssd1306.setTextXY(1, 0)
+oledssd1306.writeString("Servo2(B):")
+oledssd1306.setTextXY(2, 0)
+oledssd1306.writeString("Motor1(AB):")
+SchreibeStatus()
